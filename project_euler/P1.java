287667f78af3450a19b071affea2962295293a0c
Jainik Purohit <jainik.purohit@yahoo.com>
2018-09-03 15:11:06 -0400

    Code Explanation

--- project_euler/P1.java
+++ project_euler/P1.java
@@ -26,13 +26,19 @@ public class Solution {
         // if we take an approach of n%3 || n%5 then it will fail for very large numbers
 		// if we use int instead of long then we will have a problem with interger overflow
         
+	// we are using an approach of n(n + 1)/ 2.
+	// to compute the value of n, we are finding how many elements are divided by 3, 5 and 15 in below three statments.
+	// n - 1 is being used to conside the scenario of less than n.
         long a = (n - 1)/3;
         long b = (n - 1)/5;
         long c = (n - 1)/15;
+	    
+	// now apply the formula n(n+1)/2 and multiply sum with the factor to get total sum    
         long sum3 = 3 * a * (a + 1) / 2;
         long sum5 = 5 * b * (b + 1) / 2;
         long sum15 = 15 * c * (c + 1) / 2;
         
+	// eliminating double counting of common numbers which are divisible by both 3 and 15.    
         return sum3 + sum5 - sum15;
     }
 }
